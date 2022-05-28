f288d434393795986c691e23ec1c3537b900928b
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-05-28 23:25:43 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,15 @@
+const input = document.querySelector('div#controls input');
+const amount = input.addEventListener("input", (event) => {
+  event.currentTarget.value;  
+});
+
+function createBoxes(amount){
+  const boxes = document.querySelector('div#boxes')
+for(i=0; i<amount; i+=1){
+  boxes.append(document.createElement(div))
+};
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
