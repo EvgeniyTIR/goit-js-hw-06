b042a22e2c6980c50160367a40bbcd07d7442545
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-05-29 21:19:20 +0300

    task-10-final

--- js/task-10.js
+++ js/task-10.js
@@ -1,28 +1,27 @@
 const input = document.querySelector('div#controls input');
-const createBtn = document.querySelector('button[data-create]')
-const destroyBtn = document.querySelector('button[data-destroy]')
-const boxes = document.querySelector('div#boxes')
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('div#boxes');
 
-input.addEventListener("input", (event) => {
- let amount =  event.currentTarget.value;    
- createBtn.addEventListener("click", () => createBoxes(amount));
- destroyBtn.addEventListener("click", () => {
-   boxes.innerHTML =''
-   
-  });
+ 
+  createBtn.addEventListener("click", () => createBoxes(input.value));
+  destroyBtn.addEventListener("click", () => {
+    boxes.innerHTML =''    
+   });
+  
+
+
   
  function createBoxes(amount) {
   for(let i = 0; i < amount; i+=1){
 
     const element = document.createElement("div");
     element.style.backgroundColor = getRandomHexColor();
-    element.style.size = (30 + i*10) +'px' ;
+    element.style.height = (30 + i*10) +'px' ;
+    element.style.width= (30 + i*10) +'px' ;
     boxes.append(element);
   };
 }
-
-});
-
 
 
 function getRandomHexColor() {
