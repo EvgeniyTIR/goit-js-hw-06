3fd19bb9c6faffc44b3fb960a49027f491621fe5
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-05-29 11:43:56 +0300

    update tusk-10 -js

--- js/task-10.js
+++ js/task-10.js
@@ -1,17 +1,24 @@
 const input = document.querySelector('div#controls input');
-const amount = input.addEventListener("input", (event) => {
-  event.currentTarget.value;  
+const createBtn = document.querySelector('button[data-create]')
+const destroyBtn = document.querySelector('button[data-destroy]')
+
+
+input.addEventListener("input", (event) => {
+ let amount =  event.currentTarget.value;    
+ createBtn.addEventListener("click", () => createBoxes(amount));
+function createBoxes(amount) {
+  const boxes = document.querySelector('div#boxes')
+  for(let i = 0; i < amount; i+=1){
+    const element = document.createElement("div");
+    element.size = (30 + i*10) +'px' ;
+    boxes.append(element);
+  };
+}
 });
 
-function createBoxes(amount){
-  const boxes = document.querySelector('div#boxes')
-for(let i = 0; i < amount; i+=1){
-  const div = document.createElement(div);
-  div.size = (30 + i*10) +'px' ;
-  boxes.append(div);
-};
-}
-console.log(createBoxes())
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
