bc2ec3e71c9262c506f81b95a4814e1197003408
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-05-29 12:37:25 +0300

    update-task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,21 +1,27 @@
 const input = document.querySelector('div#controls input');
 const createBtn = document.querySelector('button[data-create]')
 const destroyBtn = document.querySelector('button[data-destroy]')
-
+const boxes = document.querySelector('div#boxes')
 
 input.addEventListener("input", (event) => {
  let amount =  event.currentTarget.value;    
  createBtn.addEventListener("click", () => createBoxes(amount));
-function createBoxes(amount) {
-  const boxes = document.querySelector('div#boxes')
+ destroyBtn.addEventListener("click", () => {
+   boxes.innerHTML =''
+   
+  });
+  
+ function createBoxes(amount) {
   for(let i = 0; i < amount; i+=1){
+
     const element = document.createElement("div");
-    element.size = (30 + i*10) +'px' ;
+    element.style.backgroundColor = getRandomHexColor();
+    element.style.size = (30 + i*10) +'px' ;
     boxes.append(element);
   };
 }
-});
 
+});
 
 
 
